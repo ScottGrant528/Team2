4d0c018dff20ee358a9096c117f5701e8c5ec453
Scott528 <hiscott2000@gmail.com>
2021-10-29 18:59:18 +0100

    copy pasta code

--- DenisLawWebApp/server.js
+++ DenisLawWebApp/server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 const session = require('express');
 var app = express();
-
+/*
 //setting the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -10,6 +10,21 @@ app.use(express.static('public'));
 app.use(session({ secret: 'example' }));
 
 app.use(express.urlencoded({extended:true}));
+*/
+
+// Require static assets from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set 'views' directory for any views 
+// being rendered res.render()
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// replace with the directory path below ./
+app.set('views', path.join(__dirname, 'views'));
 
 //login
 app.get('/', function(req, res){
