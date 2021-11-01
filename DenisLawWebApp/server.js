3053032d98993bb46516b40888384f1eb17ecf33
Scott528 <hiscott2000@gmail.com>
2021-11-01 23:18:29 +0000

    maybe working server

--- DenisLawWebApp/server.js
+++ DenisLawWebApp/server.js
@@ -1,39 +1,31 @@
-console.log('----------------------- Starting server.js -----------------------')
-// load the external applications needed 
-var express = require('express');
-const session = require('express');
-var app = express();
+//loading external applications to run the server
+const express = require('express');
+const app = express();
 
-//setting the view engine to ejs
+//server side stuff
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(session({ secret: 'example' }));
+app.listen(8080)
 
-app.use(express.urlencoded({extended:true}));
-
-//login
-app.get('/', function(req, res){
+//Default page
+app.get('/', function(req,res){
     res.render('pages/login');
-})
-
-//register
-app.get('/register', function(req, res){
-    res.render('pages/register');
-})
-
-//profile
-app.get('/profile', function(res,req){
-    res.render('pages/profile');
-})
-
-//attendance
-app.get('/attendance', function(req, res){
-    res.render('pages/attendance');
-})
-
-//default 404 page
-app.use(function(req, res, next) {
-    res.render('pages/404');
+    console.log('---- Displaying Login page ----')
 });
 
-app.listen(8080);
+//Register page
+app.get('/register', function(req, res){
+    res.render('pages/register')
+    console.log('---- Displaying Register page ----')
+});
+
+//Attendane Page
+app.get('/attendance', function(req, res){
+    res.render('pages/attendance')
+    console.log('---- Displaying Attendance page ----')
+});
+
+//Profile Page
+app.get('/profile', function(req, res){
+    res.render('pages/profile')
+    console.log('---- Displaying Profile page ----')
+});
