abc67e6dd62949e33ea40ddab0f86b5ea6ebad63
Scott528 <hiscott2000@gmail.com>
2021-11-01 23:08:58 +0000

    maybe working server????

--- DenisLawWebApp/server2.js
+++ DenisLawWebApp/server2.js
@@ -7,4 +7,22 @@ app.listen(8080)
 
 app.get('/', function(req,res){
     res.render('pages/login');
+    console.log('---- Displaying Login page ----')
+});
+
+app.get('/register', function(req, res){
+    console.log('---- Displaying Register page ----')
+});
+
+app.get('/attendance', function(req, res){
+    console.log('---- Displaying Attendance page ----')
+});
+
+app.get('/profile', function(req, res){
+    console.log('---- Displaying Profile page ----')
+});
+
+app.use(function(req, res, next) {
+    console.log('---- 404 page being displayed');
+    res.render('pages/404');
 });
