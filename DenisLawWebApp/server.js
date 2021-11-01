f7bf534e5a8271200b7ee64f2b8bac3d4cd3f2c6
Scott528 <hiscott2000@gmail.com>
2021-11-01 22:53:57 +0000

    .

--- DenisLawWebApp/server.js
+++ DenisLawWebApp/server.js
@@ -1,30 +1,14 @@
 // load the external applications needed 
 var express = require('express');
-const session = require('express');
+const session = require('express-session');
 var app = express();
 
 //setting the view engine to ejs
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 app.use(session({ secret: 'example' }));
 
 app.use(express.urlencoded({extended:true}));
-
-/*
-// Require static assets from public folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set 'views' directory for any views 
-// being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
-
-// Set view engine as EJS
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-// replace with the directory path below ./
-app.set('views', path.join(__dirname, 'views'));
 
 //login
 app.get('/', function(req, res){
@@ -50,5 +34,5 @@ app.get('/attendance', function(req, res){
 app.use(function(req, res, next) {
     res.render('pages/404');
 });
-*/
+
 app.listen(8080);
