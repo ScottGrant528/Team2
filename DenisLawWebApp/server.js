2166f7a1b2e3c3cf1b7c580cbce0e94d118dfdc6
Jared Scott <1901640@rgu.ac.uk>
2021-11-07 20:11:35 +0000

    mongodb

--- DenisLawWebApp/server.js
+++ DenisLawWebApp/server.js
@@ -2,6 +2,17 @@
 const express = require('express');
 const app = express();
 
+//mongodb initialisation
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/DLLT_Users";
+
+var db;
+MongoClient.connect(url, function(err, database){
+  if(err) throw err;
+  db = database;
+  app.listen(8080);
+});
+
 //server side stuff
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
