e9d5dbc07e6447e063f0239e419fb77b0470adb2
Jared Scott <1901640@rgu.ac.uk>
2021-11-21 16:44:26 +0000

    test json file write

--- DenisLawWebApp/views/scripts/sessions.js
+++ DenisLawWebApp/views/scripts/sessions.js
@@ -1,4 +1,21 @@
-function load(){
-    var mydata = JSON.parse(sessions);
-    alert(mydata[0].location);
-}
+$(document).ready(function(){
+
+    //Grab the JSON file containing the details for previously saved pins
+    async function getData() {
+      let url = 'assets/data/sessions.json';
+      try {
+          let res = await fetch(url);
+          return await res.json();
+          //If this fails, send an error
+      } catch (error) {
+          console.log(error);
+      }
+    }
+  
+    //Function to display previously saved pins from database on map on login
+    async function renderPins() {
+  
+      //Store the details from the grabbed JSON file
+      let pins = await getData();
+    }
+  })
