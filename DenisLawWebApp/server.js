29d7085911f4f2ea29643c690592d92dac8d1fb7
Jared Scott <1901640@rgu.ac.uk>
2021-11-21 23:28:49 +0000

    refinement

--- DenisLawWebApp/server.js
+++ DenisLawWebApp/server.js
@@ -324,7 +324,7 @@ app.post('/exportattendeeinfo', function(req, res){
                 return
             }
                 console.log('File write successful')
-                res.render('pages/Sessions')
+                res.redirect('/Sessions')
         })
     })
 })
